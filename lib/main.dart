ac7c6616649193654e489f6d450201d14b2a6435
Marcos Nastri <marcosnastri@gmail.com>
2020-06-30 08:32:10 -0300

    refeito o pdf

--- lib/main.dart
+++ lib/main.dart
@@ -5,45 +5,35 @@ void main() {
 }
 
 class ListaCustasApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new ListaScreen()
-    );
+    return new MaterialApp(home: new ListaScreen());
   }
 }
 
 class ListaScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Lista de Custas")
-        ),
+        appBar: new AppBar(title: new Text("Lista de Custas")),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            new Row(
-              children: [
-                 IconButton(
-                   icon: new Icon(Icons.cloud_download, color: Colors.green),
-                   iconSize: 44.0,
-                   onPressed: () {},
-                 ),
-                 new Column(                   
-                   crossAxisAlignment: CrossAxisAlignment.start, 
-                   children: <Widget>[
-                     Text ("Linha 1 : Custas SP"),
-                     Text ("Linha 2 : Detalhes SP")
-                   ] 
-                 ) 
-              ]
-            )  
+            new Row(children: [
+              IconButton(
+                icon: new Icon(Icons.cloud_download, color: Colors.green),
+                iconSize: 44.0,
+                onPressed: () {},
+              ),
+              new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("Linha 1 : Custas SP"),
+                    Text("Linha 2 : Detalhes SP")
+                  ])
+            ])
           ],
-        )
-       );
+        ));
   }
 }
